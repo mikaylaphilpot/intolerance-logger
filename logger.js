a44cf9580fb1b4047bfac06077896538f3007370
mikaylaphilpot <75904361+mikaylaphilpot@users.noreply.github.com>
2025-09-01 14:20:31 -0400

    set up food logging

---
+++ logger.js
@@ -0,0 +1,26 @@
+// allows logger to popup when "Log" is pressed
+function showLogger() {
+    const mealSelect = document.getElementById("mealSelect");
+    const selectedMeal = mealSelect.value;
+    let food = prompt("Log Food: ");
+    console.log(selectedMeal);
+    logFood(food, selectedMeal);
+}
+// logs given food in designated meal
+function logFood(food, selectedMeal) {
+    var mealDiv;
+    if (selectedMeal == "breakfast-select") {
+        console.log("HERE")
+        mealDiv = document.getElementById("breakfast-meal");
+    }
+    else if (selectedMeal == "lunch-select") {
+        mealDiv = document.getElementById("lunch-meal");
+    }
+    else {
+        mealDiv = document.getElementById("dinner-meal");
+    }
+    const newFood = document.createElement('p');
+    newFood.textContent = food;
+    mealDiv.appendChild(newFood);
+
+}
